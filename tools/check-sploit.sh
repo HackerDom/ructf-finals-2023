7f149724fd6c3a56a97c1c374386ccbf6dc59cd7
vaspahomov <vas2142553@gmail.com>
2023-03-20 21:07:58 +0100

    ructf-2023 (CI): try to add sploits in CI
    Signed-off-by: vaspahomov <vas2142553@gmail.com>

--- tools/check-sploit.sh
+++ tools/check-sploit.sh
@@ -7,6 +7,6 @@ TEST_FLAG="test-flag";
 
 FLAG_DATA=$(cd checkers/$SERVICE_NAME && ./$SERVICE_NAME.checker.py PUT 127.0.0.1 "$FLAG_ID" "$TEST_FLAG" $VULN_ID);
 echo "Putted flag '$FLAG_DATA'"
-FLAG_ID=$(FLAG_DATA | jq .public_flag_id)
+FLAG_ID=$(echo $FLAG_DATA | jq -r .public_flag_id)
 echo "Public flag id: '$FLAG_ID'"
 (cd sploits/$SERVICE_NAME/ && ./$SERVICE_NAME.sploit.py 127.0.0.1 "$FLAG_ID" "$TEST_FLAG");
