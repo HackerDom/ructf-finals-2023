3391fd85b39ebcd3af318d03800ec0ab4d0b5c68
Daniil Sharko <integrex@yandex.ru>
2023-03-20 13:35:13 +0300

    Add example service and checker

---
+++ checkers/example/checker.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+import json
+import random
+import sys
+import traceback
+
+import requests
+from gornilo import CheckRequest, Verdict, PutRequest, GetRequest, VulnChecker, NewChecker
+
+checker = NewChecker()
+PORT = 2222
+CHECK_URL_PATTERN = "http://{hostname}:{port}/check"
+PUT_URL_PATTERN = "http://{hostname}:{port}/put"
+GET_URL_PATTERN = "http://{hostname}:{port}/get?username={username}&token={token}"
+
+
+class ErrorChecker:
+    def __init__(self):
+        self.verdict = Verdict.OK()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if exc_type in {requests.exceptions.ConnectionError}:
+            self.verdict = Verdict.DOWN("Service is down")
+
+        elif exc_type in {requests.exceptions.HTTPError}:
+            self.verdict = Verdict.MUMBLE("Incorrect http code")
+
+        if exc_type:
+            print(exc_type)
+            print(exc_value.__dict__)
+            traceback.print_tb(exc_traceback, file=sys.stdout)
+        return True
+
+
+@checker.define_check
+async def check_service(request: CheckRequest) -> Verdict:
+    with ErrorChecker() as ec:
+        check_url = CHECK_URL_PATTERN.format(hostname=request.hostname, port=PORT)
+        r = requests.get(check_url, timeout=10)
+        r.raise_for_status()
+
+    return ec.verdict
+
+
+@checker.define_vuln("flag_id is an username")
+class CryptoChecker(VulnChecker):
+    @staticmethod
+    def put(request: PutRequest) -> Verdict:
+        with ErrorChecker() as ec:
+            rand_int = random.randint(0, 10000000)
+            username = f"user_{rand_int}"
+            token = f"token_{rand_int}"
+
+            put_url = PUT_URL_PATTERN.format(hostname=request.hostname, port=PORT)
+            data = {
+                "flag": request.flag,
+                "username": username,
+                "token": token,
+            }
+            r = requests.post(put_url, timeout=10, json=data)
+            r.raise_for_status()
+
+            flag_id = json.dumps(data)
+            ec.verdict = Verdict.OK_WITH_FLAG_ID(username, flag_id)
+        return ec.verdict
+
+    @staticmethod
+    def get(request: GetRequest) -> Verdict:
+        with ErrorChecker() as ec:
+            flag_id_json = json.loads(request.flag_id)
+
+            username = flag_id_json['username']
+            token = flag_id_json['token']
+
+            get_url = GET_URL_PATTERN.format(hostname=request.hostname, port=PORT, username=username, token=token)
+            r = requests.get(get_url, timeout=10)
+            r.raise_for_status()
+            if r.text != request.flag:
+                print(f"Wrong flag: {r.text} != {request.flag}")
+                ec.verdict = Verdict.CORRUPT('Wrong flag')
+
+        return ec.verdict
+
+
+if __name__ == '__main__':
+    checker.run()
---
+++ services/example/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	usernameToFlags := make(map[string]string)
+	usernameToTokens := make(map[string]string)
+
+	http.HandleFunc("/check", func(w http.ResponseWriter, r *http.Request) {})
+
+	http.HandleFunc("/put", func(w http.ResponseWriter, r *http.Request) {
+		data, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(500)
+			_, _ = fmt.Fprintf(w, "reading request body error: "+err.Error())
+			return
+		}
+
+		var putRequest struct {
+			Username string `json:"username"`
+			Token    string `json:"Token"`
+			Flag     string `json:"flag"`
+		}
+
+		if err := json.Unmarshal(data, &putRequest); err != nil {
+			w.WriteHeader(400)
+			_, _ = fmt.Fprintf(w, "unmarshal error: "+err.Error())
+			return
+		}
+
+		if putRequest.Flag == "" || putRequest.Username == "" || putRequest.Token == "" {
+			w.WriteHeader(400)
+			_, _ = fmt.Fprintf(w, "invalid put json")
+			return
+		}
+
+		usernameToFlags[putRequest.Username] = putRequest.Flag
+		usernameToTokens[putRequest.Username] = putRequest.Token
+	})
+
+	http.HandleFunc("/get", func(w http.ResponseWriter, r *http.Request) {
+		checkParam := func(name string) (string, error) {
+			v, ok := r.URL.Query()[name]
+			if !ok || len(v) == 0 {
+				return "", errors.New(fmt.Sprintf("invalid query params: %v is required", name))
+			}
+			return v[0], nil
+		}
+		username, err := checkParam("username")
+		if err != nil {
+			w.WriteHeader(400)
+			_, _ = w.Write([]byte(err.Error()))
+			return
+		}
+
+		token, err := checkParam("token")
+		if err != nil {
+			w.WriteHeader(400)
+			_, _ = w.Write([]byte(err.Error()))
+			return
+		}
+
+		actualToken, ok1 := usernameToTokens[username]
+		flag, ok2 := usernameToFlags[username]
+
+		if !ok1 || !ok2 {
+			w.WriteHeader(400)
+			_, _ = w.Write([]byte("user does not exist"))
+			return
+		}
+
+		if actualToken != token {
+			w.WriteHeader(401)
+			_, _ = w.Write([]byte("invalid token"))
+			return
+		}
+
+		_, _ = w.Write([]byte(flag))
+	})
+
+	log.Fatal(http.ListenAndServe(":2222", nil))
+}
