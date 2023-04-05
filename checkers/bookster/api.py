cb492f08683b7db51215f31000d10bfce5763c79
NitroLine <sergey.and.natasha@yandex.ru>
2023-04-05 16:47:09 +0500

    add check checker

--- checkers/bookster/api.py
+++ checkers/bookster/api.py
@@ -2,14 +2,24 @@ import os.path
 import time
 import traceback
 import uuid
+from urllib.parse import urlparse
 
+import requests.exceptions
 from gornilo.http_clients import requests_with_retries
 
-from checkers.bookster.utils import generate_name, generate_email, generate_password
+from utils import generate_name, generate_email, generate_password, get_hash_stream, get_hash
 
+
+class AuthError(Exception):
+    pass
+
+class ParseError(Exception):
+    pass
 
 class Api:
     def __init__(self, host, port, token=None):
+        self._host = host
+        self._port = port
         self._addr = f'http://{host}:{port}'
         self._session = requests_with_retries()
         if token:
@@ -28,7 +38,6 @@ class Api:
             )
         else:
             raise Exception("Method not found")
-
         try:
             return rsp.status_code, rsp.json()
         except:
@@ -48,6 +57,8 @@ class Api:
                 'password': password
             })
         if status == 200:
+            if "auth_token" not in data:
+                raise AuthError("No auth_token in response")
             self._session.headers.update({'Authorization':  f'Token {data["auth_token"]}'})
         return status, data
 
@@ -78,6 +89,25 @@ class Api:
         )
         return resp.status_code, resp.text
 
+    def get_file(self, url, max_size=1024 * 1024 * 1024):
+        try:
+            parsed = urlparse(url)
+            if parsed.scheme != 'http':
+                raise ParseError(f"Can't parse url: {url}")
+            if parsed.netloc != f"{self._host}:{self._port}":
+                raise ParseError(f"Can't parse url: {url}")
+            if not parsed.path.startswith('/media/'):
+                raise ParseError(f"Can't parse url: {url}")
+            if parsed.query:
+                raise ParseError(f"Can't parse url: {url}")
+            resp = self._session.get(url, stream=True)
+            if resp.status_code != 200:
+                return resp.status_code, None
+            hashed = get_hash_stream(resp, max_size)
+            return resp.status_code, hashed
+        except Exception as e:
+            raise ParseError(f"Can't parse url: {url}") from e
+
 
 if __name__ == '__main__':
     api = Api('localhost', 8900)
@@ -92,12 +122,19 @@ if __name__ == '__main__':
     print(status, data)
     test_video = os.path.join('videos', 'sample_1.avi')
     status, data = api.create_book('test', 'test flag', test_video, 'test.mp4')
+    uid = data['uid']
     print(status, data)
-    print(data['uid'])
-    status, data = api.get_book(data['uid'])
+    print(uid)
+    status, data = api.get_book(uid)
     print(status, data)
+    status, data = api.get_file(data['video'], os.path.getsize(test_video))
+    print(status, data)
+    file_hash = get_hash(test_video)
+    print(file_hash == data)
     time.sleep(5)
-    status, data = api.get_book(data['uid'])
+    status, data = api.get_book(uid)
+    print(status, data)
+    status, data = api.get_file(data['video_preview'])
     print(status, data)
 
 
