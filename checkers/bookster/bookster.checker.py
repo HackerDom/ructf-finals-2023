fdcac06e784b629ad16c4be8932ad5044f5a42c9
NitroLine <sergey.and.natasha@yandex.ru>
2023-04-05 16:47:09 +0500

    add check checker

---
+++ checkers/bookster/bookster.checker.py
@@ -0,0 +1,169 @@
+import asyncio
+import os.path
+import uuid
+
+import requests
+from gornilo import CheckRequest, Verdict, PutRequest, GetRequest, NewChecker, VulnChecker
+import random
+from api import Api, AuthError, ParseError
+from utils import generate_email, generate_name, generate_password, get_video_by_id, get_some_text, \
+    generate_video_filename, get_hash
+
+checker = NewChecker()
+PORT = 8900
+
+TOTAL_SAMPLES = 600
+SLEEP_TIMEOUT = 3
+DEFAULT_PHOTO_HASH = 'c7ae789ed7e53f805cb7a372b088b25e4e3d39972e0f6a6b5d4d013a2f10d8a3'
+
+@checker.define_check
+async def check_service(request: CheckRequest) -> Verdict:
+    api = Api(request.hostname, PORT)
+    email = generate_email()
+    username = generate_name()
+    password = generate_password()
+    try:
+
+        # index check
+        status, data = api.get_index()
+        if status != 200:
+            return Verdict.MUMBLE(f"Index failed with status {status}")
+
+        # register check
+        status, data = api.signup(email, username, password)
+        if status != 201:
+            print(data)
+            return Verdict.MUMBLE(f"Signup failed with status {status}")
+        if "username" not in data or data["username"] != username:
+            return Verdict.MUMBLE("Signup wrong username")
+        if "email" not in data or data["email"] != email:
+            return Verdict.MUMBLE("Signup wrong email")
+        if "id" not in data:
+            return Verdict.MUMBLE("Signup no id")
+
+        # login check
+        status, data = api.login(username, password)
+        if status != 200:
+            return Verdict.MUMBLE(f"Login failed with status {status}")
+
+        # check create book without photo
+        book_id = random.randint(1, TOTAL_SAMPLES)
+        book_sample = get_video_by_id(book_id)
+        text = get_some_text()
+        send_filename = generate_video_filename()
+        title_without_photo = str(uuid.uuid4())
+        status, data = api.create_book(title_without_photo, text + 'nopreview', book_sample['video'], send_filename)
+        if status != 201:
+            return Verdict.MUMBLE(f"Create book failed with status {status}")
+        if "uid" not in data:
+            return Verdict.MUMBLE("Create book no uid")
+        if "text" not in data or data["text"] != text + 'nopreview':
+            return Verdict.MUMBLE("Create book wrong text")
+        if "title" not in data or data["title"] != title_without_photo:
+            return Verdict.MUMBLE("Create book wrong title")
+        if "video" not in data:
+            return Verdict.MUMBLE("Create book no video")
+        uid_no_wait_photo = data["uid"]
+
+        # check create book with photo
+        title_with_photo = str(uuid.uuid4())
+        status, data = api.create_book(title_with_photo, text, book_sample['video'], send_filename)
+        if status != 201:
+            return Verdict.MUMBLE(f"Create book failed with status {status}")
+        if "uid" not in data:
+            return Verdict.MUMBLE("Create book no uid")
+        if "text" not in data or data["text"] != text:
+            return Verdict.MUMBLE("Create book wrong text")
+        if "title" not in data or data["title"] != title_with_photo:
+            return Verdict.MUMBLE("Create book wrong title")
+        if "video" not in data:
+            return Verdict.MUMBLE("Create book no video")
+        uid_wait_photo = data["uid"]
+
+        await asyncio.sleep(SLEEP_TIMEOUT)
+
+        # check get book with photo
+        status, data = api.get_book(uid_wait_photo)
+        if status != 200:
+            return Verdict.MUMBLE(f"Get book failed with status {status}")
+        if "uid" not in data or data["uid"] != uid_wait_photo:
+            return Verdict.MUMBLE("Get book wrong uid")
+        if "text" not in data or data["text"] != text:
+            return Verdict.MUMBLE("Get book wrong text")
+        if "title" not in data or data["title"] != title_with_photo:
+            return Verdict.MUMBLE("Get book wrong title")
+        if "video" not in data:
+            return Verdict.MUMBLE("Get book no video")
+        if "video_preview" not in data:
+            return Verdict.MUMBLE("Get book no video_preview")
+        if data["video_preview"] is None:
+            return Verdict.MUMBLE("Video preview not generated")
+        video_preview = data["video_preview"]
+        status, data = api.get_file(data["video"], os.path.getsize(book_sample['video']))
+        if status != 200:
+            return Verdict.MUMBLE(f"Get video file failed with status {status}")
+        video_hash = get_hash(book_sample['video'])
+        if data != video_hash:
+            return Verdict.MUMBLE("Get video file wrong hash")
+        status, data = api.get_file(video_preview, os.path.getsize(book_sample['photo']))
+        if status != 200:
+            return Verdict.MUMBLE(f"Get preview file failed with status {status}")
+        preview_hash = get_hash(book_sample['photo'])
+        # generated hash compare
+        if data != preview_hash:
+            return Verdict.MUMBLE("Get preview file wrong hash")
+
+        # check get book without photo
+        status, data = api.get_book(uid_no_wait_photo)
+        if status != 200:
+            return Verdict.MUMBLE(f"Get book failed with status {status}")
+        if "uid" not in data or data["uid"] != uid_no_wait_photo:
+            return Verdict.MUMBLE("Get book wrong uid")
+        if "text" not in data or data["text"] != text + 'nopreview':
+            return Verdict.MUMBLE("Get book wrong text")
+        if "title" not in data or data["title"] != title_without_photo:
+            return Verdict.MUMBLE("Get book wrong title")
+        if "video" not in data:
+            return Verdict.MUMBLE("Get book no video")
+        if "video_preview" not in data:
+            return Verdict.MUMBLE("Get book no video_preview")
+        if data["video_preview"] is None:
+            return Verdict.MUMBLE("Video preview not generated")
+        video_preview = data["video_preview"]
+        status, data = api.get_file(data["video"], os.path.getsize(book_sample['video']))
+        if status != 200:
+            return Verdict.MUMBLE(f"Get video file failed with status {status}")
+        video_hash = get_hash(book_sample['video'])
+        if data != video_hash:
+            return Verdict.MUMBLE("Get video file wrong hash")
+        status, data = api.get_file(video_preview, 326981)
+        if status != 200:
+            return Verdict.MUMBLE(f"Get preview file failed with status {status}")
+        # generated hash compare
+        if data != DEFAULT_PHOTO_HASH:
+            return Verdict.MUMBLE("Get preview file wrong hash")
+    except AuthError:
+        return Verdict.MUMBLE("Login error")
+    except ParseError:
+        return Verdict.MUMBLE("Wrong url to download file")
+    except requests.ConnectionError:
+        return Verdict.DOWN('connection error')
+    except requests.Timeout:
+        return Verdict.DOWN('timeout error')
+    except requests.RequestException:
+        return Verdict.MUMBLE('http error')
+    return Verdict.OK()
+
+@checker.define_vuln("flag_id is uid")
+class XSSChecker(VulnChecker):
+    @staticmethod
+    def put(request: PutRequest) -> Verdict:
+        return Verdict.OK_WITH_FLAG_ID("email1", "next_id1")
+
+    @staticmethod
+    def get(request: GetRequest) -> Verdict:
+        return Verdict.OK()
+
+
+if __name__ == '__main__':
+    checker.run()
--- checkers/bookster/utils.py
+++ checkers/bookster/utils.py
@@ -1,3 +1,6 @@
+import csv
+import hashlib
+import os
 import random
 import secrets
 import string
@@ -33,3 +36,66 @@ def generate_name():
     alphabet = string.ascii_letters
     name = ''.join(secrets.choice(alphabet) for i in range(20))
     return name + str(uuid.uuid4())
+
+
+def get_video_by_id(video_id):
+    with open('answers.csv', 'r') as f:
+        reader = csv.reader(f)
+        next(reader)
+        for row in reader:
+            if int(row[0]) == video_id:
+                return {
+                    'video': row[1],
+                    'photo': row[2],
+                    'hash': row[3],
+                }
+def get_some_text():
+    total_size = os.path.getsize('bulgakov.txt')
+    with open('bulgakov.txt', 'r', encoding='utf-8', errors='ignore') as f:
+        f.seek(random.randint(1, total_size - 501))
+        return f.read(500)
+
+
+
+VIDEO_EXTENSION = ['avi', 'mp4', 'mkv', 'mov', 'flv', 'wmv', 'webm']
+
+
+def generate_video_filename():
+    filename = ''.join(secrets.choice(string.ascii_letters) for i in range(10))
+    if random.randint(0, 10) > 7:
+        filename += 'concat'
+    filename += ''.join(secrets.choice(string.ascii_letters) for i in range(5))
+    filename += '.'
+    filename += secrets.choice(VIDEO_EXTENSION)
+    return filename
+
+
+BUF_SIZE = 8192
+
+
+def get_hash(filename):
+    if not filename:
+        return None
+    hashed = hashlib.sha256()
+    with open(filename, 'rb') as f:
+        for chunk in iter(lambda: f.read(BUF_SIZE), b''):
+            hashed.update(chunk)
+    return hashed.hexdigest()
+
+
+def get_hash_stream(stream, max_size):
+    if not stream:
+        return None
+    hashed = hashlib.sha256()
+    size = 0
+    for chunk in stream.iter_content(chunk_size=BUF_SIZE):
+        hashed.update(chunk)
+        size += len(chunk)
+        if size > max_size:
+            break
+    return hashed.hexdigest()
+
+
+if __name__ == '__main__':
+    print(get_hash('./../../services/booksrter/service/media/images/default_lolo.jpg'))
+    print(os.path.getsize('./../../services/booksrter/service/media/images/default_lolo.jpg'))
