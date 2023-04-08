292d2788f7c3efd6188e31b95e4b12c6dce05a17
Alexander Bersenev <bay@hackerdom.ru>
2023-04-09 01:00:09 +0500

    patch do specific setup

--- vuln_image/digital_ocean_specific_setup.sh
+++ vuln_image/digital_ocean_specific_setup.sh
@@ -1,8 +1,9 @@
-
 #!/bin/bash
 
 # Replace /24 netmask with /25 one
 sed -i '/nic_type = nic.get/a \        if nic_type == "private": nic["ipv4"]["netmask"] = "255.255.255.128"' /usr/lib/python3/dist-packages/cloudinit/sources/helpers/digitalocean.py
+# Newer DigitalOcean uses OpenStack's mechanics
+sed -i '/subnets.append(subnet)/a \            if subnet.get("address", "").startswith("10.6"): subnet["netmask"] = "255.255.255.128"' /usr/lib/python3/dist-packages/cloudinit/sources/helpers/openstack.py
 
 # Set PasswordAuthentication yes
 sed -i '/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes' /etc/ssh/sshd_config
