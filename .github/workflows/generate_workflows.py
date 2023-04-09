4f4fa98b61105eba2d626d74d2dcc955a3386430
vaspahomov <vas2142553@gmail.com>
2023-04-09 17:13:31 +0200

    ructf-2023 (CI): separate check service, checker, sploit
    Signed-off-by: vaspahomov <vas2142553@gmail.com>

--- .github/workflows/generate_workflows.py
+++ .github/workflows/generate_workflows.py
@@ -25,7 +25,17 @@ on:
         required: false
         default: "no"
 jobs:
-  check_{service}:
+  check_service_{service}:
+    name: Check service {service}
+    runs-on: ubuntu-20.04
+    steps:
+    - name: Checkout repo
+      uses: actions/checkout@v2
+    - name: Run prebuilt hook
+      run: if [ -f services/{service}/before_image_build.sh ]; then (cd ./services/{service} && ./before_image_build.sh); fi
+    - name: Setup {service}
+      run: (cd ./services/{service} && docker-compose pull && docker-compose build && docker-compose up --build -d)
+  check_checker_{service}:
     name: Check {service}
     runs-on: ubuntu-20.04
     steps:
@@ -43,14 +53,30 @@ jobs:
       run: if [ -f checkers/{service}/requirements.txt ]; then python -m pip install -r checkers/{service}/requirements.txt; fi
     - name: Test checker on service
       run: (cd ./checkers/{service} && ./{service}.checker.py TEST 127.0.0.1)
+  check_sploit_{service}:
+    name: Check {service}
+    runs-on: ubuntu-20.04
+    steps:
+    - name: Checkout repo
+      uses: actions/checkout@v2
+    - name: Run prebuilt hook
+      run: if [ -f services/{service}/before_image_build.sh ]; then (cd ./services/{service} && ./before_image_build.sh); fi
+    - name: Setup {service}
+      run: (cd ./services/{service} && docker-compose pull && docker-compose build && docker-compose up --build -d)
+    - name: Prepare python for checker
+      uses: actions/setup-python@v2
+      with:
+        python-version: 3.9
+    - name: Setup checker libraries
+      run: if [ -f checkers/{service}/requirements.txt ]; then python -m pip install -r checkers/{service}/requirements.txt; fi
     - name: Setup sploit libraries
       run: if [ -f sploits/{service}/requirements.txt ]; then python -m pip install -r sploits/{service}/requirements.txt; fi
     - name: Test sploit on service
       run: (./tools/check-sploit.py {service})
   update_{service}:
     name: Deploy service using ansible to first teams
-    needs: check_{service}
-    runs-on: deployer
+    needs: [check_service_{service}, check_checker_{service}, check_sploit_{service}]
+    runs-on: self-hosted
     if: ${{{{ false }}}} # disable on game just in case
     steps:
     #- name: install ansible
