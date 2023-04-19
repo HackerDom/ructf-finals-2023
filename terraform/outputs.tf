77849a53186d3b37598044aff75f78a94b1d66ce
1MiKHalyCH1 <bykoun@gmail.com>
2023-04-20 00:01:25 +0500

    [terraform] build ansible inventories from tf outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,9 @@
 output "cs-master-public-ip" {
   value = digitalocean_droplet.cs-master.ipv4_address
 }
-output "cs-master-private-ip" {
-  value = digitalocean_droplet.cs-master.ipv4_address_private
-}
 
 output "cs-checker-public-ip" {
   value = digitalocean_droplet.cs-checker.ipv4_address
-}
-output "cs-checker-private-ip" {
-  value = digitalocean_droplet.cs-checker.ipv4_address_private
 }
 
 output "gh-workflow-public-ip" {
@@ -18,4 +12,40 @@ output "gh-workflow-public-ip" {
 
 output "vpn-public-ip" {
   value = digitalocean_droplet.vpn.ipv4_address
+}
+
+
+resource "local_file" "cs_ansible_inventory" {
+  filename = "../ansible/cs/inventory.cfg"
+  content  = <<-EOF
+[master]
+cs-master ansible_host=${digitalocean_droplet.cs-master.ipv4_address} private_addr=${digitalocean_droplet.cs-master.ipv4_address_private}
+
+[flags]
+cs-master
+
+[db]
+cs-master
+
+[monitoring]
+cs-master
+
+[checkers]
+ch1 ansible_host=${digitalocean_droplet.cs-checker.ipv4_address} private_addr=${digitalocean_droplet.cs-checker.ipv4_address_private} private_ip=${digitalocean_droplet.cs-checker.ipv4_address_private}
+
+[cs:children]
+master
+flags
+db
+checkers
+monitoring
+EOF
+}
+
+resource "local_file" "vpn_ansible_inventory" {
+  filename = "../ansible/vpn/inventory.cfg"
+  content  = <<-EOF
+[vpn]
+vpn.a ansible_host=${digitalocean_droplet.vpn.ipv4_address}
+EOF
 }
