cc04eba5bbd8f4aa4b37e227225a483788a50946
Danil Kazakov <dkazakov@alt-fund.com>
2023-04-20 22:31:51 +0500

    add alpha beta 0.0.1 version

---
+++ services/lost-museum/internal/common/errors.go
@@ -0,0 +1,9 @@
+package common
+
+import "errors"
+
+var (
+	ErrAlreadyExists   = errors.New("already exists")
+	ErrNotExists       = errors.New("not exists")
+	ErrInvalidPassword = errors.New("invalid password")
+)
