7b50b6eed4316c38878d4f70fbe646e851cb883a
e-postovalov <e-postovalov@yandex-team.ru>
2023-04-22 00:35:16 +0500

    Bix bug fixes (fix flops)

--- checkers/scp/scp.checker.py
+++ checkers/scp/scp.checker.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.11
 
+import json
 import random
 import re
 import string
@@ -22,7 +23,7 @@ def get_token(url: str):
         "login": random_str(),
         "password": random_str()
     }
-    resp = requests_with_retries(1).post(welcome, json=user_data)
+    resp = requests_with_retries().post(welcome, json=user_data)
     while resp.status_code == 409:
         user_data = {
             "login": random_str(),
@@ -41,7 +42,7 @@ async def check_service(request: CheckRequest) -> Verdict:
 
         # checking welcome [get]
 
-        resp = requests_with_retries().get(url+"welcome")
+        resp = requests_with_retries().get(url + "welcome")
         if resp.status_code != 200:
             return Verdict.MUMBLE("Verdict: MUMBLE bad welcome")
 
@@ -49,14 +50,14 @@ async def check_service(request: CheckRequest) -> Verdict:
 
         token = get_token(url)
         if not token:
-            return Verdict.CORRUPT("Verdict: CORRUPT could not create user")
+            return Verdict.MUMBLE("Verdict: MUMBLE could not create user")
 
         # checking create new document
 
         n = random.randint(1, 1000)
         doc_flag = f'doc_scp{n}_{random_str()} <- "{random_str()}";'
         doc_fields = []
-        for i in range(random.randint(2, 15)):
+        for i in range(random.randint(2, 3)):
             field = random_str()
             doc_field = f'doc_scp{n}_{field} <- "{random_str()}";'
             sec = ''
@@ -71,9 +72,9 @@ async def check_service(request: CheckRequest) -> Verdict:
         if resp.status_code == 400:
             return Verdict.MUMBLE("Verdict: MUMBLE could not get list of docs")
         if resp.status_code == 500:
-            return Verdict.CORRUPT("Verdict: CORRUPT got internal server error")
+            return Verdict.MUMBLE("Verdict: MUMBLE got internal server error")
         if resp.status_code != 200:
-            return Verdict.CORRUPT("Verdict: CORRUPT unexpected status code")
+            return Verdict.MUMBLE("Verdict: MUMBLE unexpected status code " + str(resp.status_code))
 
         # checking list of docs
 
@@ -85,15 +86,15 @@ async def check_service(request: CheckRequest) -> Verdict:
         if resp.status_code == 400:
             return Verdict.MUMBLE("Verdict: MUMBLE could not get list of docs")
         if resp.status_code == 500:
-            return Verdict.CORRUPT("Verdict: CORRUPT got internal server error")
+            return Verdict.MUMBLE("Verdict: MUMBLE got internal server error")
         if resp.status_code != 200:
-            return Verdict.CORRUPT("Verdict: CORRUPT unexpected status code")
+            return Verdict.MUMBLE("Verdict: MUMBLE unexpected status code " + str(resp.status_code))
         resp_data = resp.json()
         if resp_data["type"] != "PLAIN":
             return Verdict.MUMBLE("Verdict: MUMBLE weird answer")
         raw_list = resp_data["body"].strip()
         if not raw_list:
-            return Verdict.MUMBLE("Verdict: MUMBLE empty list of documents")  # todo: think about first round
+            return Verdict.MUMBLE("Verdict: MUMBLE empty list of documents")
         docs_with_users = []
         for pair in raw_list.split("\n"):
             doc, user = pair.split(":")[0].strip(), pair.split(":")[1].strip()
@@ -115,11 +116,10 @@ async def check_service(request: CheckRequest) -> Verdict:
         if resp.status_code == 400:
             return Verdict.MUMBLE("Verdict: MUMBLE could not get random doc")
         if resp.status_code == 500:
-            return Verdict.CORRUPT("Verdict: CORRUPT got internal server error")
-        return Verdict.CORRUPT("Verdict: CORRUPT unexpected status code")
+            return Verdict.MUMBLE("Verdict: MUMBLE got internal server error")
+        return Verdict.MUMBLE("Verdict: MUMBLE unexpected status code " + str(resp.status_code))
 
     except Exception as e:
-        print(e)
         return Verdict.DOWN("timeout")
 
 
@@ -130,9 +130,10 @@ class GameChecker(VulnChecker):
         try:
             url = f"http://{request.hostname}:8081/"
             flag = request.flag
-            doc_id = request.public_flag_id
-            token = request.flag_id
-            doc_id = f'@docId <- {doc_id};'
+            flag_id_json = json.loads(request.flag_id)
+            _id = flag_id_json["id"]
+            doc_id = f'@docId <- {_id};'
+            token = flag_id_json["token"]
             doc = '@doc <- {(@docId) -> get};'
             res = '{(@doc) -> result};'
             ql = doc_id + doc + res
@@ -144,21 +145,24 @@ class GameChecker(VulnChecker):
             if resp.status_code == 400:
                 return Verdict.MUMBLE("Verdict: MUMBLE could not execute query")
             if resp.status_code == 500:
-                return Verdict.CORRUPT("Verdict: CORRUPT got internal server error")
+                return Verdict.MUMBLE("Verdict: MUMBLE got internal server error")
             if resp.status_code != 200:
-                return Verdict.CORRUPT("Verdict: CORRUPT unexpected status code")
+                return Verdict.MUMBLE("Verdict: MUMBLE unexpected status code " + str(resp.status_code))
             resp_data = resp.json()
-            if resp_data["type"] != "HTML":
-                return Verdict.MUMBLE("Verdict: MUMBLE got weird document")
-            possible_flags = re.findall(flag_re, resp_data["body"])
+            if resp_data.get("type", "") != "HTML":
+                return Verdict.CORRUPT("Verdict: CORRUPT got weird document")
+            resp_body = resp_data.get("body")
+            if not resp_body:
+                return Verdict.CORRUPT("Verdict: CORRUPT response without body")
+            possible_flags = re.findall(flag_re, resp_body)
             if not possible_flags:
-                return Verdict.MUMBLE("Verdict: MUMBLE could not find my secret")
+                return Verdict.CORRUPT("Verdict: CORRUPT could not find my secret")
             for possible_flag in possible_flags:
                 if possible_flag == flag:
                     return Verdict.OK()
-            return Verdict.MUMBLE("Verdict: MUMBLE could not find my secret")
-        except Exception:
-            return Verdict.DOWN("Verdict: DOWN could not read secrets")
+            return Verdict.CORRUPT("Verdict: CORRUPT could not find my secret")
+        except Exception as e:
+            return Verdict.DOWN("Verdict: DOWN could not read secrets" + str(e))
 
     @staticmethod
     def put(request: PutRequest) -> Verdict:
@@ -173,7 +177,7 @@ class GameChecker(VulnChecker):
             doc_flag = f'doc_scp{n}_secret <- "{flag}";'
             sec_flag = '{(scp' + str(n) + ', "secret") -> secret};'
             doc_fields = []
-            for i in range(random.randint(2, 15)):
+            for i in range(random.randint(2, 3)):
                 field = random_str()
                 doc_field = f'doc_scp{n}_{field} <- "{random_str()}";'
                 sec = ''
@@ -194,14 +198,15 @@ class GameChecker(VulnChecker):
             if resp.status_code == 500:
                 return Verdict.CORRUPT("Verdict: CORRUPT got internal server error")
             if resp.status_code != 200:
-                return Verdict.CORRUPT("Verdict: CORRUPT unexpected status code")
+                return Verdict.CORRUPT("Verdict: CORRUPT unexpected status code " + str(resp.status_code))
             resp_data = resp.json()
             doc_id = resp_data["body"]
             if not doc_id:
                 return Verdict.MUMBLE("Verdict: MUMBLE could not get document id")
-            return Verdict.OK_WITH_FLAG_ID(doc_id, token)
-        except Exception:
-            return Verdict.DOWN("Verdict: DOWN could not write secrets")
+            return Verdict.OK_WITH_FLAG_ID(doc_id, json.dumps({"id": doc_id, "token": token}))
+            # return Verdict.OK_WITH_FLAG_ID(doc_id, token)
+        except Exception as e:
+            return Verdict.DOWN("Verdict: DOWN could not write secrets" + str(e))
 
 
 if __name__ == "__main__":
