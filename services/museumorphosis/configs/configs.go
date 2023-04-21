d95e0d312398de99ac6f03616f5c351232c7c97b
Alexander Moshkov <9aq360zqmuf1@mail.ru>
2023-04-22 02:29:44 +0500

    [museumorphosis] update service, checker, sploit

--- services/museumorphosis/configs/configs.go
+++ services/museumorphosis/configs/configs.go
@@ -2,26 +2,18 @@ package configs
 
 import (
 	"github.com/google/uuid"
-	"github.com/spf13/viper"
 	"os"
 )
 
 type Configs struct {
-	Port      int `mapstructure:"APP_PORT"`
+	Port      int
 	JWTSecret string
 }
 
 var Cfg *Configs
 
 func Init() (*Configs, error) {
-	viper.SetConfigFile(".env")
-	if err := viper.ReadInConfig(); err != nil {
-		return nil, err
-	}
-	Cfg = &Configs{}
-	if err := viper.Unmarshal(Cfg); err != nil {
-		return nil, err
-	}
+	Cfg = &Configs{Port: 8000}
 	secret, err := generateSecretIfNotExits()
 	if err != nil {
 		return nil, err
