47438ef48c9cd479a46b137008987912f4045582
capitanbanana <d.lukshto@gmail.com>
2023-04-22 05:15:23 +0300

    lost_museum fucking fix
    ochko porvu za eto

--- services/lost_museum/back/internal/infra/repo.go
+++ services/lost_museum/back/internal/infra/repo.go
@@ -209,7 +209,7 @@ func (r *Repo) GetRequestsList(ctx context.Context, username string) ([]string, 
 }
 
 func (r *Repo) Clean(ctx context.Context) error {
-	_, err := r.col.UpdateMany(ctx, map[string]string{}, map[string]map[string]string{"$unset": {
+	_, err := r.col.UpdateMany(ctx, map[string]string{}, map[string]map[string]string{{"$lt" : new Date(Date.now() - 2*60*60 * 1000) }, "$unset": {
 		"jokes": "", "friends": "", "friend_requests": ""}})
 	if err != nil {
 		return fmt.Errorf("error while cleaning: %w", err)
